b49d11b3fa20d8adc492c317607eee35e0bb1624
joeysola <joeysolak@gmail.com>
2016-10-29 19:58:13 -0400

    Writing To Text
    Writing To Text Completed

--- RWBT/src/rwbt/RWBT.java
+++ RWBT/src/rwbt/RWBT.java
@@ -6,6 +6,7 @@
  */
 package rwbt;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
@@ -17,9 +18,6 @@ import java.util.Scanner;
  */
 public class RWBT {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) throws FileNotFoundException {
         
         //Set Variables
@@ -28,12 +26,14 @@ public class RWBT {
         String bort;
         String rorw;
         String info;
-        String menu;
-        String cont;
+        String menu = "Y";
+        String cont = "Y";
         final String BINARY = ".dat";
         final String TEXT = ".txt";
         
         
+        do{
+            
         
         //Ask for information
         Scanner keyboard = new Scanner(System.in);
@@ -41,47 +41,94 @@ public class RWBT {
         //Filename
         System.out.print("Enter the filename: ");
         name = keyboard.nextLine();
-        System.out.println("");
+        
         
         //Binary or Text
-        System.out.print("Choose binary or text:(b/t)");
+        System.out.print("Choose binary or text:(b/t) ");
         bort = keyboard.nextLine();
-        System.out.println("");
         
         if (bort.equals("b")){
             filename = name + BINARY;
         }
             else {
             filename = name + TEXT;
-                    
-                    }
-            
-        
-        
+        }
         
         //Read or Write
-        System.out.print("Choose read or write:(r/w)");
+        System.out.print("Choose read or write:(r/w) ");
         rorw = keyboard.nextLine();
-        System.out.println("");
+        
+        
+        //Decision Time
+        
+        if (rorw.equalsIgnoreCase("R") && bort.equalsIgnoreCase("T")){
+        
+            //Read from Text File
+            Scanner read = null;
+            PrintWriter rw = null;
+            
+            try{
+            read = new Scanner(new FileInputStream(filename));
+            rw = new PrintWriter(new FileOutputStream(filename, true));
+            }
+            catch (FileNotFoundException e){
+                System.out.println("Error opening your file. The program will now exit.");
+                System.exit(0);
+            }
+        
+            //Scan through lines of text file
+            String line = null;
+            int count = 0;
+            System.out.println("File contains:");
+            while (read.hasNextLine()){
+                line = read.nextLine();
+                count++;
+                rw.println(count + " " + line);
+                System.out.println(line); //Display 
+            }
+            read.close(); //Close
+            } 
         
         
         
+        else if (rorw.equalsIgnoreCase("W") && bort.equalsIgnoreCase("T")){
+            
+            do {
+            //Enter Information
+            System.out.print("Enter a line of information to write: ");
+            info = keyboard.nextLine();
+
+
+            //Create and Write Text
+            PrintWriter write = null;
+
+            try{
+            write = new PrintWriter(new FileOutputStream(filename, true));
+            }
+            catch (FileNotFoundException e){
+                System.out.println("Error opening your file. The program will now exit.");
+                System.exit(0);    
+            }
+
+            write.println(info);//Write to Text File
+            write.close();//Close 
+            
+            System.out.print("Would you like to enter another line?(y/n) ");
+            menu = keyboard.nextLine();
+            
+            } while (menu.equalsIgnoreCase("Y"));
+
+                      
+            }
+            
+        
+            System.out.print("\nWould you like to continue the program?(y/n) ");
+            cont = keyboard.nextLine();
+            
+        } while(cont.equalsIgnoreCase("Y"));
         
         
-        
-        
-        
-       
-        
-        
-        //Create and Write
-        PrintWriter write = null;
-        write = new PrintWriter(new FileOutputStream(filename));
-        
-        write.println("Write Line 1");
-        write.println("Write Line 2");
-        write.close();
-        
+      }
     }
     
-}
+
--- ReadMe.txt
+++ ReadMe.txt
@@ -2,8 +2,7 @@ Assignment# 6 Part 2
 ===========================================================================
 
 Statement of Work:
-
-
+Write a program that reads and writes from binary or text files.
 
 
 
@@ -11,3 +10,13 @@ Statement of Work:
 ===========================================================================
 
 Pseudocode:
+
+Dialogue
+Enter the filename:
+Choose binary or text:
+Choose read or write:
+Enter a line of information to write:
+
+Create Class for Binary and Text
+
+
