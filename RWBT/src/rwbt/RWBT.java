c427680712557e9c53057f2089c4eede090b9cad
joeysola <joeysolak@gmail.com>
2016-10-29 21:26:18 -0400

    Final Update
    Done

--- RWBT/src/rwbt/RWBT.java
+++ RWBT/src/rwbt/RWBT.java
@@ -6,9 +6,13 @@
  */
 package rwbt;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
@@ -18,14 +22,14 @@ import java.util.Scanner;
  */
 public class RWBT {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         
         //Set Variables
         String name;
         String filename = "";
         String bort;
         String rorw;
-        String info;
+        String info = "TESTING 1 2 3";
         String menu = "Y";
         String cont = "Y";
         final String BINARY = ".dat";
@@ -59,8 +63,9 @@ public class RWBT {
         rorw = keyboard.nextLine();
         
         
-        //Decision Time
+        //DECISION TREE
         
+        //IF READ AND TEXT
         if (rorw.equalsIgnoreCase("R") && bort.equalsIgnoreCase("T")){
         
             //Read from Text File
@@ -90,14 +95,14 @@ public class RWBT {
             } 
         
         
-        
+        //IF WRITE AND TEXT
         else if (rorw.equalsIgnoreCase("W") && bort.equalsIgnoreCase("T")){
             
             do {
             //Enter Information
             System.out.print("Enter a line of information to write: ");
             info = keyboard.nextLine();
-
+            menu = "Y";
 
             //Create and Write Text
             PrintWriter write = null;
@@ -120,15 +125,72 @@ public class RWBT {
 
                       
             }
-            
         
-            System.out.print("\nWould you like to continue the program?(y/n) ");
-            cont = keyboard.nextLine();
+        //IF WRITE AND BINARY
+        else if(rorw.equalsIgnoreCase("W") && bort.equalsIgnoreCase("B") ){
+        
+            do {
+            //Enter Information
+            System.out.print("Enter a line of information to write: ");
+            info = keyboard.nextLine();
+            menu = "Y";
+            
+            //Write Binary File
+        
+        DataOutputStream bWrite = null;
+        
+        try{
+            bWrite =
+                new DataOutputStream(new FileOutputStream(filename, true));
+            }
+            catch (IOException e){
+                System.out.println("Error opening your file. The program will now exit.");
+                System.exit(0);    
+            }
+
+            bWrite.writeUTF(info);//Write to Text File
+            //bWrite.close();
+            
+            System.out.print("Would you like to enter another line?(y/n) ");
+            menu = keyboard.nextLine();
+    
+            } while (menu.equalsIgnoreCase("Y"));
+        }
+            
+        //IF READ AND BINARY
+        else if(rorw.equalsIgnoreCase("R") && bort.equalsIgnoreCase("B")){
+         
+        //Read Binary File
+         String bOutput = "BLAH";
+         DataInputStream bRead;
+        
+         
+         try{
+             bRead = new DataInputStream(new FileInputStream(filename));
+           try{ 
+            while (bRead.available()>0){
+             bOutput = bRead.readUTF();
+             System.out.println(bOutput);
+            }
+           }
+             catch(EOFException e){
+                    System.out.println("No more lines in the file.");
+                    }bRead.close();
+         }
+         
+         catch (FileNotFoundException e){
+             System.out.println("Error opening your file. The program will now exit.");
+             System.exit(0);
+             
+         } 
+                }
+
+          System.out.print("\nWould you like to continue the program?(y/n) ");
+            cont = keyboard.nextLine(); 
             
         } while(cont.equalsIgnoreCase("Y"));
-        
-        
-      }
+
+         }
+         
     }
-    
 
--- ReadMe.txt
+++ ReadMe.txt
@@ -11,12 +11,17 @@ Write a program that reads and writes from binary or text files.
 
 Pseudocode:
 
-Dialogue
-Enter the filename:
-Choose binary or text:
-Choose read or write:
-Enter a line of information to write:
+Write code for Reading and Writing Text Files and Binary Files
 
-Create Class for Binary and Text
+Make if statments for:
+Read Text
+Write Text - Loop until user say no
+Read Binary
+Write Binary - Loop until user say no
+
+Loop until user says no
+
+
+
 
 
